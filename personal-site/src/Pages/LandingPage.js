05b329d36eb888b1cf20403b426291dfcf3580f1
Philip Gilhespy <philip@gilhespy.net>
2024-05-12 15:22:44 -0700

    got title font correct

---
+++ personal-site/src/Pages/LandingPage.js
@@ -0,0 +1,16 @@
+import '../Styles/Pages.css';
+import '../Styles/Containers.css';
+import '../Styles/Text.css';
+
+function LandingPage() {
+    return (
+        <div className="Landing-page">
+            <div className="Centered-container-horz-vert">
+                <span className="Inter-black Text-large Text-white Drop-shadow-black">PHILIP GILHESPY</span>
+                <span className="Inter-regular Text-small Text-white">WEB DESIGN    PROJECTS    CONTACT</span>
+            </div>
+        </div>
+    );
+}
+
+export default LandingPage;
