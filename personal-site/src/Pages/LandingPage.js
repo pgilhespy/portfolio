ed770677d9ce91ed0aa672782ccbd15c11de65f0
Philip Gilhespy <philip@gilhespy.net>
2024-05-12 18:14:07 -0700

    added subtle animation on load

--- personal-site/src/Pages/LandingPage.js
+++ personal-site/src/Pages/LandingPage.js
@@ -1,12 +1,13 @@
 import '../Styles/Pages.css';
 import '../Styles/Containers.css';
 import '../Styles/Text.css';
+import '../Styles/Animations.css';
 
 function LandingPage() {
     return (
         <div className="Landing-page">
             <div className="Centered-container-horz-vert">
-                <span className="Inter-black Text-large Text-white Drop-shadow-black">PHILIP GILHESPY</span>
+                <span className="Inter-black Text-large Text-white Drop-shadow-black Letters-seperate">PHILIP GILHESPY</span>
                 <span className="Inter-regular Text-small Text-white">WEB DESIGN    PROJECTS    CONTACT</span>
             </div>
         </div>
