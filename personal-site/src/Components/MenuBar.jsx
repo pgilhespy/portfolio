de8be67a10d2f59b6bc44d4d5a984b6dce43ae63
Philip Gilhespy <philip@gilhespy.net>
2024-06-09 19:56:41 -0700

    fixed invisble text issue

--- personal-site/src/Components/MenuBar.jsx
+++ personal-site/src/Components/MenuBar.jsx
@@ -15,14 +15,11 @@ const MenuBar = ({ scrollPos }) => {
 
     return (
         <div className='Menu-bar Even-spread-to-edges Menu-front' style={{visibility:`${visibility}`}}>
-            <div className="Centered-container-horz-vert Middle">
-                <div className='Even-spread-to-edges'>
+                <div className='Even-spread-to-edges' style={{width: "723.1px"}}>
                     <ButtonBoxHighlight displayText={"ABOUT"} scrollPos={scrollPos} />
                     <ButtonBoxHighlight displayText={"WORK"} scrollPos={scrollPos} />
                     <ButtonBoxHighlight displayText={"CONTACT"} scrollPos={scrollPos} />
                 </div>
-                <span className="Inter-black Spacing-tight Text-large Text-white Drop-shadow-black Inactive" style={{visibility: "hidden"}}>PHILIP GILHESPY</span>
-            </div>
         </div>
     );
 }
