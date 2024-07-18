9d5f0d485537c56f8c2925f5c803e8cf7185d29e
Philip Gilhespy <philip@gilhespy.net>
2024-07-17 19:47:14 -0700

    scaled about intro images

--- personal-site/src/Pages/AboutIntroPage.js
+++ personal-site/src/Pages/AboutIntroPage.js
@@ -12,11 +12,45 @@ import getTextSize from '../Utils/GetTextSize';
 
 function AboutIntroPage({ scrollPos }) {
     const {width, height} = useWindowDimensions();
+    const screenSize = getTextSize(width);
+    var floatingImage1;
+    var floatingImage2; 
+
+    if (screenSize == "window") {
+        floatingImage1 = [1, 1, 1];
+        floatingImage2 = [1, 1, 1];
+    }
+    else if (screenSize == "ipad") {
+        floatingImage1 = [-70, 20, 0.7];
+        floatingImage2 = [-100, 20, 0.7];
+    }
+    else {
+        floatingImage1 = [-130, 50, 0.5];
+        floatingImage2 = [-190, 50, 0.5];
+    }
     
     return (
         <div id="about" className="About-pages">
-            <FloatingImage image={image} left={30} top={10} rotation={0} scale={0.6} scrollPos={scrollPos} invertParalax={-1} pageNumber={0} />
-            <FloatingImage image={image} left={160} top={10} rotation={0} scale={-0.6} scrollPos={scrollPos} invertParalax={-1} pageNumber={0} />
+            <FloatingImage 
+                image={image} 
+                left={30+floatingImage1[0]} 
+                top={10+floatingImage1[1]} 
+                rotation={0} 
+                scale={0.6*floatingImage1[2]} 
+                scrollPos={scrollPos} 
+                invertParalax={-1} 
+                pageNumber={0} 
+            />
+            <FloatingImage 
+                image={image} 
+                left={160+floatingImage2[0]} 
+                top={10+floatingImage2[1]} 
+                rotation={0} 
+                scale={-0.6*floatingImage2[2]} 
+                scrollPos={scrollPos} 
+                invertParalax={-1} 
+                pageNumber={0} 
+            />
             <div className="Centered-container-horz-vert Middle Margin-to-center">
                 <span className={`Inter-black Spacing-${getTextSize(width)}-medium Text-${getTextSize(width)}-medium Text-white Drop-shadow-black`}>
                     I’m a web designer with computer science and video editing skills who can take your site to the <ScrollHighlightText scrollPos={scrollPos} displayText={"next level."} />
