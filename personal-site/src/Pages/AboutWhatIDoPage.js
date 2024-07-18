dbc202a1cd468ba9eb75dd6a9ded7be28da7fe75
Philip Gilhespy <philip@gilhespy.net>
2024-07-17 20:11:37 -0700

    about what i do graphic scaled

--- personal-site/src/Pages/AboutWhatIDoPage.js
+++ personal-site/src/Pages/AboutWhatIDoPage.js
@@ -12,10 +12,31 @@ import getTextSize from '../Utils/GetTextSize';
 
 function AboutWhatIDoPage( {scrollPos} ) {
     const {width, height} = useWindowDimensions();
+    const screenSize = getTextSize(width);
+    var floatingVideo; 
+
+    if (screenSize == "window")
+        floatingVideo = [1, 1, 1];
+
+    else if (screenSize == "ipad")
+        floatingVideo = [1, 0.95, 0.8];
+
+    else
+        floatingVideo = [1, 0.9, 0.6];
     
     return (
         <div className="About-pages">
-            <FloatingVideo video={bubbleVideo} right={-200} bottom={450} rotation={30} scale={1.3} scrollPos={scrollPos} invertParalax={1} zInd={25} pageNumber={1} />
+            <FloatingVideo
+                video={bubbleVideo} 
+                right={-200*floatingVideo[0]} 
+                bottom={450*floatingVideo[1]} 
+                rotation={30} 
+                scale={1.3*floatingVideo[2]} 
+                scrollPos={scrollPos} 
+                invertParalax={1} 
+                zInd={25} 
+                pageNumber={1} 
+            />
             <span className={`Inter-regular Spacing-${getTextSize(width)}-wide Text-${getTextSize(width)}-small Text-white`} >WHAT I DO</span>
             <div className="Centered-container-horz-left-align Middle Margin-to-center Drop-shadow-container" style={{ background: "#414C52", }}>
                 <FadeoutCaptionText mainText={"WEB DESIGN"} subText={"Create and optimize website content with code and other tools"}/>
