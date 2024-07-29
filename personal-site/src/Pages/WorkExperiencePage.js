01248ce09a41cf0431322c6edb8591810d14d4eb
Philip Gilhespy <philip@gilhespy.net>
2024-07-28 17:16:08 -0700

    update standardfusion text

--- personal-site/src/Pages/WorkExperiencePage.js
+++ personal-site/src/Pages/WorkExperiencePage.js
@@ -17,7 +17,7 @@ function WorkExperiencePage() {
             <span className={`Inter-regular Spacing-${getTextSize(width)}-wide Text-${getTextSize(width)}-small Text-white`} >MY EXPERIENCE</span>
             <div className="Centered-container-horz-left-align Middle Drop-shadow-container" style={{ background: "#C8D2D7", }}>
                 <FadeoutCaptionText mainText={"ANTHEM"} subText={"Edited the show reel for a company event"}/>
-                <FadeoutCaptionText mainText={`${getTextSize(width) == "phone" ? "STANDARD-FUSION" : "STANDARDFUSION"}`} subText={"Worked as a web developer"} />
+                <FadeoutCaptionText mainText={`${getTextSize(width) == "phone" ? "STANDARD FUSION" : "STANDARDFUSION"}`} subText={"Worked as a web developer"} />
                 <FadeoutCaptionText mainText={"METROPOLITAN"} subText={"Created a short social media ad"} />
                 <FadeoutCaptionText mainText={"CERULEAN SOLUTIONS"} subText={"Developed a new website for the company"} />
                 <FadeoutCaptionText mainText={"..."} />
