d9061882486cc5310ad6c10e6819c985dbc1cf4a
Philip Gilhespy <philip@gilhespy.net>
2024-08-01 15:22:28 -0700

    added auto fadeout to captions for mobile

--- personal-site/src/Components/FadeoutCaptionText.jsx
+++ personal-site/src/Components/FadeoutCaptionText.jsx
@@ -4,31 +4,71 @@ import '../Styles/Components.css';
 import '../Styles/Text.css';
 import '../Styles/Animations.css';
 import '../Styles/Globals.css';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import MobileDetect from 'mobile-detect';
 import getTextSize from '../Utils/GetTextSize';
 import useWindowDimensions from '../Utils/UseWindowDimensions';
 
 const FadeoutCaptionText = ({ mainText, subText }) => {
     const [isHovered, setIsHovered] = useState(false);
     const {height, width} = useWindowDimensions();
- 
+    const md = new MobileDetect(navigator.userAgent);
+    const mainTextRef = useRef(null);
+
+    // Auto fadeout if mobile device
+    // if (md.mobile()) {
+    //     if (mainTextRef.current) {
+    //         const mainTextBoundingBox = mainTextRef.current.getBoundingClientRect();
+    //         if ( mainTextBoundingBox.top > ((height/2) - (height/8)) )
+    //             setIsHovered(true);
+    //     }
+    // }
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (md.mobile())
+                if (mainTextRef.current) {
+                    const mainTextBoundingBox = mainTextRef.current.getBoundingClientRect();
+                    const isVisible = (
+                        mainTextBoundingBox.top >= (height - 2*mainTextBoundingBox.height) / 2 &&
+                        mainTextBoundingBox.bottom <= (height + 2*mainTextBoundingBox.height) / 2
+                    );
+                    setIsHovered(isVisible);
+                }
+        };
+    
+        window.addEventListener('scroll', handleScroll);
+        // Initial check on component mount
+        handleScroll();
+    
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     const handleMouseEnter = () => {
-        setIsHovered(true);
+        if ( !md.mobile() )
+            setIsHovered(true);
     };
  
     const handleMouseLeave = () => {
-        setIsHovered(false);
+        if ( !md.mobile() )
+            setIsHovered(false);
     };
  
+    const maintextOpacity = isHovered ? '0.2' : '1';
+    const maintextBlur = isHovered ? '2px' : '0px';
     const subtextOpacity = isHovered ? '1' : '0';
     const subtextBlur = isHovered ? '0px' : '2px';
 
     return (
         <div className='Even-spread-to-edges Position-relative'>
             <span 
+                ref={mainTextRef}
                 onMouseEnter={handleMouseEnter} 
                 onMouseLeave={handleMouseLeave} 
                 className={`Inter-black Spacing-${getTextSize(width)}-tight Line-height-${getTextSize(width)} Text-${getTextSize(width)}-large Text-white Tight-fadeout-text`}
+                style={{opacity: `${maintextOpacity}`, filter: `blur(${maintextBlur})`}}
             >
                 { mainText }
             </span>
