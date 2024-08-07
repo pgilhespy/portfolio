4296981a27dd807c0dff87f15d7a4639a87c3628
Philip Gilhespy <philip@gilhespy.net>
2024-08-07 14:20:39 -0700

    fixed page sizes at 100px intervals

--- personal-site/src/Utils/GetPageHeight.js
+++ personal-site/src/Utils/GetPageHeight.js
@@ -1,9 +1,13 @@
 
 function getPageHeight( height ) {
     if (height < 500)
-        return 500;
+        return 500; 
 
-    return height;
+    let adjustedHeight = height / 100;
+    adjustedHeight = Math.ceil(adjustedHeight);
+    adjustedHeight *= 100;
+
+    return adjustedHeight;
 };
 
 export default getPageHeight;
